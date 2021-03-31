adb03c44bab3f9e9295332e8e76639e1b3a39d7b
Aleksander Majos <aleksander.majos@gmail.com>
2021-03-31 18:52:17 +0200

    BIGAI ENV + WORKING WITH QM

--- ENGINE/DATALAKE/__init__.py
+++ ENGINE/DATALAKE/__init__.py
@@ -1,11 +1,14 @@
 """
 BIGAI DATALAKE
 """
-from BIGAI.API.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Write_Brokers_Opening_Times, Write_Brokers_Closing_Times
-from BIGAI.API.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Open_Time_To_Existing_Chart, Open_Time_To_New_Chart
-from BIGAI.API.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Close_Time_To_Existing_Chart, Close_Time_To_New_Chart
-from BIGAI.API.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Average_OpenTimes
-from BIGAI.API.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Average_CloseTimes
+from ENGINE.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Write_Brokers_Opening_Times, Write_Brokers_Closing_Times
+from ENGINE.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Open_Time_To_Existing_Chart, Open_Time_To_New_Chart
+from ENGINE.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Close_Time_To_Existing_Chart, Close_Time_To_New_Chart
+from ENGINE.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Average_OpenTimes
+from ENGINE.DATALAKE.TIMESERIES.Opening_Closing_Times_MT45 import Average_CloseTimes
+from ENGINE.DATALAKE.TIMESERIES import Oanda_Data
+from ENGINE.DATALAKE.TIMESERIES.Oanda_Data import exampleAuth
+
 __all__ = [
     "Write_Brokers_Opening_Times",
     "Write_Brokers_Closing_Times",
