b711eb911ee4762e3fe74e9afb9656ca5701e296
Aleksander Majos <aleksander.majos@gmail.com>
2021-05-04 13:38:03 +0200

    BIGAI DF TO TIMESERIES

---
+++ ENGINE/DATALAKE/TIMESERIES/Data_to_PytorchForecasting.py
@@ -0,0 +1,37 @@
+import os
+import warnings
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
+import torch
+
+from pytorch_forecasting import Baseline, NBeats, TimeSeriesDataSet
+from pytorch_forecasting.data import NaNLabelEncoder
+from pytorch_forecasting.data.examples import generate_ar_data
+from pytorch_forecasting.metrics import SMAPE
+
+def DF_to_TSDataSet(data):
+    data.insert(0,'series','True')
+    max_encoder_length = 60
+    max_prediction_length = 20
+
+    training_cutoff = data["time_idx"].max() - max_prediction_length
+
+    context_length = max_encoder_length
+    prediction_length = max_prediction_length
+
+    training = TimeSeriesDataSet(
+        data[lambda x: x.time_idx <= training_cutoff],
+        time_idx="time_idx",
+        target="pred",
+        group_ids=["series"],
+        time_varying_unknown_reals=["pred"],
+        max_encoder_length=context_length,
+        max_prediction_length=prediction_length
+    )
+
+    validation = TimeSeriesDataSet.from_dataset(training, data, min_prediction_idx=training_cutoff + 1)
+    batch_size = 128
+    train_dataloader = training.to_dataloader(train=True, batch_size=batch_size, num_workers=0)
+    val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size, num_workers=0)
+    return train_dataloader, val_dataloader
--- ENGINE/DATALAKE/TIMESERIES/Oandzina.py
+++ ENGINE/DATALAKE/TIMESERIES/Oandzina.py
@@ -1,6 +1,20 @@
 from Oanda_Data import OandaData
 from ENGINE.DATALAKE.TIMESERIES.OHLC_Manipulate import OHLC_DF_to_REGRESSION_C, OHLC_DF_to_CLASSIFICATION_C
+from ENGINE.DATALAKE.TIMESERIES.Data_to_PytorchForecasting import DF_to_TSDataSet
+import pandas as pd
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
+import torch
+
+from pytorch_forecasting import Baseline, NBeats, TimeSeriesDataSet
+from pytorch_forecasting.data import NaNLabelEncoder
+from pytorch_forecasting.data.examples import generate_ar_data
+from pytorch_forecasting.metrics import SMAPE
 OD = OandaData()
 data = OD.GetData("2021-01-01T00:00:00Z", "2021-04-21T00:00:00Z", "EUR_USD", "M15", "DF")
-df = OHLC_DF_to_CLASSIFICATION_C(data)
-oko = 4
+df = OHLC_DF_to_REGRESSION_C(data)
+train_dataloader, val_dataloader = DF_to_TSDataSet(df)
+actuals = torch.cat([y[0] for x, y in iter(val_dataloader)])
+baseline_predictions = Baseline().predict(val_dataloader)
+value = SMAPE()(baseline_predictions, actuals)
+print(value)
