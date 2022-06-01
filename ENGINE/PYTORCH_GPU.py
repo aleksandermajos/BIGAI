66c10632775d55ebc6097b13fb83b40b88a61b88
aleks <aleksander.majos@gmail.com>
2022-06-01 20:19:39 +0200

    BIGAI RL FIRST

--- ENGINE/PYTORCH_GPU.py
+++ ENGINE/PYTORCH_GPU.py
@@ -1,7 +1,8 @@
 import torch
 
 def detect_gpu_pytorch():
-    print('IS AVAILABLE? :')
+    print(torch.__version__)
+    print('IS GPU AVAILABLE? :')
     print(torch.cuda.is_available())
     print('HOW MANY DEVICES? :')
     print(torch.cuda.device_count())
@@ -9,3 +10,5 @@ def detect_gpu_pytorch():
         print(torch.cuda.get_device_name(i))
         print('Allocated:', round(torch.cuda.memory_allocated(1) / 1024 ** 3, 1), 'GB')
         print('Cached:   ', round(torch.cuda.memory_reserved(1) / 1024 ** 3, 1), 'GB')
+
+detect_gpu_pytorch()
