98773fae34f0180efe6bd7c146543cf1eb230586
aleks <aleksander.majos@gmail.com>
2022-07-10 12:49:54 +0200

    QM TODO

--- ENGINE/PYTORCH_GPU.py
+++ ENGINE/PYTORCH_GPU.py
@@ -8,7 +8,5 @@ def detect_gpu_pytorch():
     print(torch.cuda.device_count())
     for i in range(torch.cuda.device_count()):
         print(torch.cuda.get_device_name(i))
-        print('Allocated:', round(torch.cuda.memory_allocated(1) / 1024 ** 3, 1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(1) / 1024 ** 3, 1), 'GB')
 
 detect_gpu_pytorch()
