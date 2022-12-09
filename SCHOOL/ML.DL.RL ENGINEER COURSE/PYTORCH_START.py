3b77c95c687eeabd32440e80a1dff00fb618cf11
QM <aleksander.majos@gmail.com>
2022-12-09 10:01:21 +0100

    BIGAI BEGINNING FOR SCHOOL

--- SCHOOL/ML.DL.RL ENGINEER COURSE/PYTORCH_START.py
+++ SCHOOL/ML.DL.RL ENGINEER COURSE/PYTORCH_START.py
@@ -63,6 +63,17 @@ C=torch.cat([A,B], axis=0)
 S=torch.stack([A,B],axis=1)
 
 
+def compute_z(a, b, c):
+    r1 = torch.sub(a, b)
+    r2 = torch.mul(r1, 2)
+    z = torch.add(r2, c)
+    return z
+
+print('Scalar Inputs:', compute_z(torch.tensor(1), torch.tensor(2), torch.tensor(3)))
+print('Rank 1 Inputs:', compute_z(torch.tensor([1]), torch.tensor([2]), torch.tensor([3])))
+print('Rank 2 Inputs:', compute_z(torch.tensor([[1]]), torch.tensor([[2]]), torch.tensor([[3]])))
+
+
 
 
 
