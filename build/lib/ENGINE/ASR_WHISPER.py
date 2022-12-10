0cf993d5c7c6844ef669ffc11b049b22f6c5e1d4
aleks <aleksander.majos@gmail.com>
2022-12-10 18:09:01 +0100

    BIGAI REC SOUND SPEAKERS

---
+++ build/lib/ENGINE/ASR_WHISPER.py
@@ -0,0 +1,51 @@
+import os
+import torch
+import whisper
+import re
+from BUSINESS.NLP.FILES_OPS import get_all_names
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+lang = 'Spanish'
+learning_path = '../DATA/PHRASES/LEARNING/'+lang
+
+class WhisperModel():
+        def __init__(self,size='medium', lang='polish'):
+            self.model = whisper.load_model(size)
+            self.options = dict(language=lang, beam_size=5, best_of=5)
+            self.transcribe_options = dict(task="transcribe", **self.options)
+            self.translate_options = dict(task="translate", **self.options)
+            self.last_transcribe =""
+            self.last_translate =""
+
+
+        def transcribe_file(self,path,file):
+            filename, file_extension = os.path.splitext(file)
+            full_input_path = path + '/' + file
+            transcription = self.model.transcribe(full_input_path, **self.transcribe_options)["text"]
+            translation = self.model.transcribe(full_input_path, **self.translate_options)["text"]
+            print(transcription)
+            print(translation)
+            full_output_path = path + '/' + transcription + file_extension
+            full_output_path = re.sub("[$@&!?]", "", full_output_path)
+            os.rename(full_input_path, full_output_path)
+            self.last_transcribe = transcription
+            self.last_translate = translation
+
+
+        def transcribe_all_in_dir(self,path,fe ='.wav'):
+            list_of_files = get_all_names(path)
+            for name in list_of_files:
+                filename, file_extension = os.path.splitext(name)
+                if file_extension == fe:
+                    full_input_path = path + '/' + name
+                    transcription = self.model.transcribe(full_input_path, **self.transcribe_options)["text"]
+                    translation = self.model.transcribe(full_input_path, **self.translate_options)["text"]
+                    print(transcription)
+                    print(translation)
+                    full_output_path = path + '/' + transcription + file_extension
+                    full_output_path = re.sub("[$@&!?]", "", full_output_path)
+                    os.rename(full_input_path, full_output_path)
+                    self.last_transcribe = transcription
+                    self.last_translate = translation
+            return transcription
+
---
+++ build/lib/ENGINE/TTS_DE_SILERO.py
@@ -0,0 +1,26 @@
+import torch
+import os
+import simpleaudio as sa
+
+class TTS_DE():
+
+    def __init__(self,speaker='eva_k'):
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = torch.device(DEVICE)
+        self.local_file = 'DATA/MODELS/TTS/model_de_silero.pt'
+        print(os.getcwd())
+        self.model = torch.package.PackageImporter(self.local_file).load_pickle("tts_models", "model")
+        self.model.to(self.device)
+        self.sample_rate = 48000
+        self.speaker=speaker
+
+    def create_and_save(self,text):
+        audio_paths = self.model.save_wav(text=text,
+                             speaker=self.speaker,
+                             sample_rate=self.sample_rate)
+        filename = 'test.wav'
+        wave_obj = sa.WaveObject.from_wave_file(filename)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()  # Wait until sound has finished playing
+
+
