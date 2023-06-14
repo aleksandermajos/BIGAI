a6a0df8515d077acbd22e487444dbf67192c65ed
Aleksander Majos <aleksander.majos@gmail.com>
2023-06-14 16:08:24 +0200

    STARTING DIARY_WINDOW.py

---
+++ ENGINE/JAX.py
@@ -0,0 +1,18 @@
+import jax
+print(jax.default_backend())
+print(jax.devices())
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+from pathlib import Path
+from jax import pmap
+from flax.jax_utils import replicate
+from flax.training.common_utils import shard
+from PIL import Image
+
+from huggingface_hub import notebook_login
+from diffusers import FlaxStableDiffusionPipeline
+
+
