fc16f48212eab1d0a0b8dcc1408d7e09fdb1133c
aleks <aleksander.majos@gmail.com>
2023-09-19 12:26:26 +0200

    BIGAI ENGINE ALOHA DE FILES

--- ENGINE/GENERATE_SENTENCES.py
+++ ENGINE/GENERATE_SENTENCES.py
@@ -26,24 +26,57 @@ def give_n_sentences_m_word(words_list,sentences=2,key='de'):
             sentences) + ' sentences with each word from list of words' + wo + ' in Spanish language with english translations'
         if key == 'fr': text = 'Give me ' + str(
             sentences) + ' sentences with each word from list of words' + wo + ' in French language with english translations'
-        bot_text = chat_bot.talk(text)
-        bot_text_list = bot_text.split("\n")
-        bot_text_list = [x for x in bot_text_list if x != '']
+
+        bot_text =''
+        while bot_text=='':
+            try:
+                bot_text = chat_bot.talk(text)
+                bot_text_list = bot_text.split("\n")
+                bot_text_list = [x for x in bot_text_list if x != '']
+            except:
+                print("Something went wrong when calling OPENAI GPT")
+
     return bot_text_list
 
-def generate_sentences_from_words(num_sent=6,key='de'):
-    words = give_words(0,1001,key=key)
+def generate_sentences_from_words(num_sent=4,dif=1,key='de'):
+    words = give_words(0,1001,dif=dif,key=key)
     n_sentences = []
 
     for slice in range(int(len(words)/num_sent)):
 
         words_slice = words[slice*num_sent:slice*num_sent + num_sent]
-        n_sentences = n_sentences+ give_n_sentences_m_word(words_slice,sentences=num_sent,key='es')
+        n_sentences = n_sentences+ give_n_sentences_m_word(words_slice,sentences=num_sent,key=key)
         print(slice)
     df = pd.DataFrame(n_sentences,columns =['Sentences'])
-    if key=='es': df.to_excel("SENTENCES_ES_1000_V1.xlsx", index=False)
-    if key == 'de': df.to_excel("SENTENCES_DE_1000_V1.xlsx", index=False)
-    if key == 'fr': df.to_excel("SENTENCES_fr_1000_V1.xlsx", index=False)
+    if key == 'de':
+        if dif == 1: df.to_excel("SENTENCES_DE_1000_V1.xlsx", index=False)
+        if dif == 2: df.to_excel("SENTENCES_DE_2000_V1.xlsx", index=False)
+        if dif == 3: df.to_excel("SENTENCES_DE_3000_V1.xlsx", index=False)
+        if dif == 4: df.to_excel("SENTENCES_DE_4000_V1.xlsx", index=False)
+        if dif == 5: df.to_excel("SENTENCES_DE_5000_V1.xlsx", index=False)
+        if dif == 6: df.to_excel("SENTENCES_DE_6000_V1.xlsx", index=False)
+        if dif == 7: df.to_excel("SENTENCES_DE_7000_V1.xlsx", index=False)
+        if dif == 8: df.to_excel("SENTENCES_DE_8000_V1.xlsx", index=False)
+
+    if key=='es':
+        if dif==1: df.to_excel("SENTENCES_ES_1000_V1.xlsx", index=False)
+        if dif == 2: df.to_excel("SENTENCES_ES_2000_V1.xlsx", index=False)
+        if dif == 3: df.to_excel("SENTENCES_ES_3000_V1.xlsx", index=False)
+        if dif == 4: df.to_excel("SENTENCES_ES_4000_V1.xlsx", index=False)
+        if dif == 5: df.to_excel("SENTENCES_ES_5000_V1.xlsx", index=False)
+        if dif == 6: df.to_excel("SENTENCES_ES_6000_V1.xlsx", index=False)
+        if dif == 7: df.to_excel("SENTENCES_ES_7000_V1.xlsx", index=False)
+        if dif == 8: df.to_excel("SENTENCES_ES_8000_V1.xlsx", index=False)
+
+    if key == 'fr':
+        if dif==1: df.to_excel("SENTENCES_fr_1000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_2000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_3000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_4000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_5000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_6000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_7000_V1.xlsx", index=False)
+        if dif == 1: df.to_excel("SENTENCES_fr_8000_V1.xlsx", index=False)
 
     return df
 
@@ -80,7 +113,8 @@ def generate_sentences_from_en(key='de'):
 
     return en
 
-words = give_words(0,1000,dif=2,key='de')
+
+
 
 
 
--- ENGINE/NEW_LANG_PIPELINE.py
+++ ENGINE/NEW_LANG_PIPELINE.py
@@ -1,4 +1,8 @@
-from GENERATE_SENTENCES import give_words, give_n_sentences_m_word
-import pandas as pd
+from GENERATE_SENTENCES import generate_sentences_from_words
 
 
+key_list = ["de",'es','fr']
+
+for key in key_list:
+    for dif in range(2,8):
+        generate_sentences_from_words(num_sent=10,dif=dif,key=key)
