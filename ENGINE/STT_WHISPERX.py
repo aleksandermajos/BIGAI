e58ecbaf18f6829b4ab7ea0b564dba9d1ad34a3a
aleks <aleksander.majos@gmail.com>
2024-03-11 10:38:00 +0100

    BIGAI 111 START

--- ENGINE/STT_WHISPERX.py
+++ ENGINE/STT_WHISPERX.py
@@ -1,32 +1,87 @@
 from KEY_HF import provide_key
 import moviepy.editor as mp
 import whisperx
+import math
+
+def format_time(seconds):
+
+    hours = math.floor(seconds / 3600)
+    seconds %= 3600
+    minutes = math.floor(seconds / 60)
+    seconds %= 60
+    milliseconds = round((seconds - math.floor(seconds)) * 1000)
+    seconds = math.floor(seconds)
+    formatted_time = f"{hours:02d}:{minutes:02d}:{seconds:01d},{milliseconds:03d}"
+
+    return formatted_time
+
+
+def generate_subtitle_file(language, segments, file_name,speaker = False):
+    translator = pipeline('translation', model=model_trans, tokenizer=tokenizer, src_lang='en', tgt_lang=language,
+                          max_length=400)
+    subtitle_file = f"{file_name}.{language}.srt"
+    text = ""
+    for index, segment in enumerate(segments):
+        segment_start = format_time(segment['start'])
+        segment_end = format_time(segment['end'])
+        text += f"{str(index + 1)} \n"
+        text += f"{segment_start} --> {segment_end} \n"
+        if speaker: text += f"{segment['speaker']+':'}"
+
+        output = translator(segment['text'])
+        translated_text = output[0]['translation_text']
+
+        text += f"{translated_text} \n"
+        text += "\n"
+
+    f = open(subtitle_file, "w",encoding="utf-8")
+    f.write(text)
+    f.close()
+
+    return subtitle_file
 
 
 hf_key = provide_key()
-device = "cuda"
+device = "cuda" #tu sobie wpisz cpu zamiast cuda i poleci na procku
 audio_file = "audio.mp3"
 batch_size = 16
-compute_type = "float16"
+if device =='cpu':
+    compute_type = "float"
+else:
+    compute_type = "float16"
 model = whisperx.load_model("large-v3", device, compute_type=compute_type)
+speaker = False # ta flaga ustawia czy dodaje do napisow dzielenie na poszczegolnych ludzi w filmie
 
 
-my_clip = mp.VideoFileClip(r"movie_1.mp4")
-my_clip.audio.write_audiofile(r"audio.mp3")
-audio = whisperx.load_audio(audio_file)
+
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+
+model_name = 'jbochi/madlad400-3b-mt'
+model_trans = T5ForConditionalGeneration.from_pretrained(model_name, device_map="auto")
+tokenizer = T5Tokenizer.from_pretrained(model_name)
 
 
-languages = ["pl", "ko", "ru", 'uk']
-for language in languages:
-    result = model.transcribe(audio, batch_size=batch_size, task="transcribe", language= language)
-    print(result["segments"]) # before alignment
-    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-    result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-    print(result["segments"]) # after alignment
-    '''
-    diarize_model = whisperx.DiarizationPipeline(use_auth_token=hf_key, device=device)
-    diarize_segments = diarize_model(audio)
-    result = whisperx.assign_word_speakers(diarize_segments, result)
-    print(diarize_segments)
-    print(result["segments"])
-    '''
+
+languages = ['rus_Cyrl','pol_Latn','kor_Hang','ukr_Cyrl']  # lista jezykow
+movies = ["movie_1.mp4","movie_2.mp4"] #lista filmow
+for movie in movies:
+    my_clip = mp.VideoFileClip(movie)
+    my_clip.audio.write_audiofile(r"audio.mp3")
+    audio = whisperx.load_audio(audio_file)
+    for language in languages:
+        result = model.transcribe(audio, batch_size=batch_size, task="transcribe", language= 'en')
+        print(result["segments"]) # before alignment
+        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+        result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+        print(result["segments"]) # after alignment
+
+        if speaker:
+            diarize_model = whisperx.DiarizationPipeline(use_auth_token=hf_key, device=device)
+            diarize_segments = diarize_model(audio)
+            result = whisperx.assign_word_speakers(diarize_segments, result)
+            print(diarize_segments)
+            print(result["segments"])
+
+        segments = result['segments']
+
+        generate_subtitle_file(language=language, segments=segments, file_name=movie, speaker=speaker)
--- FRONT/ALOHAPP/APP_CONVERSATIONS_AI.py
+++ FRONT/ALOHAPP/APP_CONVERSATIONS_AI.py
@@ -1,92 +1,99 @@
+import argparse
 import flet as ft
-import platform
-from ENGINE.ALOHAPP_USER_FILES_WORDS import load_de, load_es, load_fr
+from ENGINE.ALOHAPP_USER_FILES_WORDS import load_de, load_es, load_fr, load_it, load_pt, load_ro, load_ca, load_en, load_pl, load_ja, load_ko
+from ENGINE.ALOHAPP_USER_FILES_WORDS import load_ar, load_ru, load_zh, load_tr, load_sv, load_da, load_nl, load_fa, load_he
 from FRONT.ALOHAPP.CONTAINERS import generate_words_buttons,create_words_container, create_conversation_container
 from ENGINE.ALOHAPP_GIVE_WORDS import give_words_conv
 from ENGINE.ALOHAPP_OPENAI_CHATGPT import ChatGPT
-from ENGINE.STT_WHISPER import WhisperModel
-import spacy
+from ENGINE.ALOHAPP_STT_WHISPER_LIVE import Whisper_live
 import spacy_stanza
-
+import sounddevice as sd
 
 
 def main(page: ft.Page):
+    page.main_language = 'ja'
+    print(sd.query_devices())
+
+
+
     page.title = "CONVERSATIONS_AI"
-    main_language = 'DE'
-    page.main_language = 'de'
+    langs = ['de','en','es','fr','it','pl','pt','ca','ro','ar','ja','ko','ru','zh','tr','sv','nl','da', 'fa','he']
+    tts = ['silero','ellabs','playht']
+    api_hub = ['edenai']
+    params = {
+        "langs": langs,
+        "tts": tts,
+        'api_hub': api_hub,
+        'words_number': 15,
+        'word_times' : 4,
+        'repeat_word' : 4
+    }
+    parser = argparse.ArgumentParser(description="", allow_abbrev=True)
+    # Positional args
+    parser.add_argument('-m', '--model', default='small', type=str, help="Whisper.cpp model, default to %(default)s")
+    parser.add_argument('-ind', '--input_device', type=int, default=None,
+                        help=f'Id of The input device (aka microphone)\n'
+                             f'available devices {Whisper_live.available_devices()}')
+    parser.add_argument('-st', '--silence_threshold', default=16,
+                        help=f"he duration of silence after which the inference will be running, default to %(default)s")
+    parser.add_argument('-bd', '--block_duration', default=30,
+                        help=f"minimum time audio updates in ms, default to %(default)s")
 
-
-
-    words_number = 15
-    word_times = 4
-    repeat_word = 4
-
-
-
-    def language_selection(main_language='DE'):
-        if main_language == 'DE':
-            page.user_words_dictionary_de = load_de()
-            words_fin, words_pos = give_words_conv(page.user_words_dictionary_de,
-                                                                    words_number=words_number,
-                                                                    word_times=word_times,
-                                                                    lang='DE'
-                                                                   )
-            page.lemma = spacy_stanza.load_pipeline("de")
-            os = platform.system()
-            if os == 'Darwin':
-                page.whisper_model = WhisperModel(size='small', lang='german')
-            if os == 'Windows' or os == 'Linux':
-                page.whisper_model = WhisperModel(size='medium', lang='german')
-
-
-
-        if main_language == 'FR':
-            page.user_words_dictionary_fr = load_fr()
-            words_fin, words_pos = give_words_conv(page.user_words_dictionary_fr,
-                                                                    words_number=words_number,
-                                                                    word_times=word_times,
-                                                                    lang='FR'
-                                                                   )
-            page.lemma = spacy_stanza.load_pipeline("fr")
-            os = platform.system()
-            if os == 'Darwin':
-                page.whisper_model = WhisperModel(size='small', lang='french')
-            if os == 'Windows' or os == 'Linux':
-                page.whisper_model = WhisperModel(size='medium', lang='french')
-
-        if main_language == 'ES':
-            page.user_words_dictionary_es = load_es()
-            words_fin, words_pos = give_words_conv(page.user_words_dictionary_es,
-                                                                    words_number=words_number,
-                                                                    word_times=word_times,
-                                                                    lang='ES'
-                                                                   )
-            page.lemma = spacy_stanza.load_pipeline("es")
-            os = platform.system()
-            if os == 'Darwin':
-                page.whisper_model = WhisperModel(size='small', lang='spanish')
-            if os == 'Windows' or os == 'Linux':
-                page.whisper_model = WhisperModel(size='medium', lang='spanish')
-
-        page.chat_bot = ChatGPT()
-
-
-        page.words_buttons = generate_words_buttons((words_fin))
-        page.words_column, page.words_container = create_words_container(page.words_buttons)
-        page.conversation_column, page.conversation_container = create_conversation_container()
-        page.repeat_table = [repeat_word] * words_number
-        page.repeat_table_full = [repeat_word] * words_number
-        page.repeat_words_full = words_fin
+    args = parser.parse_args()
 
 
 
 
-    language_selection(main_language)
+    page.chat_bot = ChatGPT()
+
+    if page.main_language == 'de': page.user_words_dictionary = load_de()
+    if page.main_language == 'es': page.user_words_dictionary = load_es()
+    if page.main_language == 'fr': page.user_words_dictionary = load_fr()
+    if page.main_language == 'it': page.user_words_dictionary = load_it()
+    if page.main_language == 'pt': page.user_words_dictionary = load_pt()
+    if page.main_language == 'ro': page.user_words_dictionary = load_ro()
+    if page.main_language == 'ca': page.user_words_dictionary = load_ca()
+    if page.main_language == 'pl': page.user_words_dictionary = load_pl()
+    if page.main_language == 'en': page.user_words_dictionary = load_en()
+    if page.main_language == 'ko': page.user_words_dictionary = load_ko()
+    if page.main_language == 'ja': page.user_words_dictionary = load_ja()
+    if page.main_language == 'ar': page.user_words_dictionary = load_ar()
+    if page.main_language == 'ru': page.user_words_dictionary = load_ru()
+    if page.main_language == 'zh': page.user_words_dictionary = load_zh()
+    if page.main_language == 'tr': page.user_words_dictionary = load_tr()
+    if page.main_language == 'sv': page.user_words_dictionary = load_sv()
+    if page.main_language == 'nl': page.user_words_dictionary = load_nl()
+    if page.main_language == 'da': page.user_words_dictionary = load_da()
+    if page.main_language == 'fa': page.user_words_dictionary = load_fa()
+    if page.main_language == 'he': page.user_words_dictionary = load_he()
+
+    page.lemma = spacy_stanza.load_pipeline(page.main_language)
+    words_fin, words_pos = give_words_conv(page.user_words_dictionary,
+                                           words_number=params['words_number'],
+                                           word_times=params['word_times'],
+                                           lang=page.main_language
+                                           )
+
+    page.words_buttons = generate_words_buttons((words_fin))
+    page.words_column, page.words_container = create_words_container(page.words_buttons)
+    page.conversation_column, page.conversation_container = create_conversation_container()
+    page.repeat_table = [params['repeat_word']] * params['words_number']
+    page.repeat_table_full = [params['repeat_word']] * params['words_number']
+    page.repeat_words_full = words_fin
+
+
     row = ft.Row(
         [page.conversation_container, page.words_container], expand=True)
     page.add(row)
     page.theme_mode = ft.ThemeMode.DARK
 
+    my_assistant = Whisper_live(main_page=page,
+                                language=page.main_language,
+                                model=args.model,
+                                input_device=11,
+                                silence_threshold=args.silence_threshold,
+                                block_duration=args.block_duration,
+                                commands_callback=print)
+    my_assistant.start()
+
 ft.app(target=main)
-
-
