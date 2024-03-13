fd27abe791adfbc155950c023305edf65355c252
aleks <aleksander.majos@gmail.com>
2024-03-13 18:21:27 +0100

    BIGAI SCHOOL COURSES START

---
+++ ENGINE/GPU_TF.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# Num GPUs Available:  1
+
+tf.config.list_physical_devices('GPU')
+# [PhysicalDevice(name='/physical_device:GPU:0', device_type='GPU')]
+
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
