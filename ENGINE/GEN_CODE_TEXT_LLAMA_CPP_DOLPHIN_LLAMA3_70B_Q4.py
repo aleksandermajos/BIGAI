241d237e4e53e54801b93efd296be531b60adb4e
aleksander majos <aleksander.majos@gmail.com>
2024-04-30 14:14:09 +0200

    LLAMA2 VS LLAMA3 PLAY TICKTACTOE.BIGAI SCHOOL

--- ENGINE/GEN_CODE_TEXT_LLAMA_CPP_DOLPHIN_LLAMA3_70B_Q4.py
+++ ENGINE/GEN_CODE_TEXT_LLAMA_CPP_DOLPHIN_LLAMA3_70B_Q4.py
@@ -6,6 +6,6 @@ path = path_beginning+"MODELS/TEXT/"
 model_path = path+'dolphin-2.9-llama3-70b.Q4_0.gguf'
 
 
-llm = Llama(model_path=model_path,n_gpu_layers=78)
+llm = Llama(model_path=model_path,n_gpu_layers=75)
 output = llm("Q: Write in 100 sentences best things to do in life... A: ", max_tokens=320, stop=["Q:", "\n"], echo=True)
 print(output)
