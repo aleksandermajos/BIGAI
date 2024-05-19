91053825ffa2cab0a08ff8ed395c34428db675cc
aleksander majos <aleksander.majos@gmail.com>
2024-05-19 14:02:10 +0200

    BIGAI ENGINE CHATGPT

--- ENGINE/ALOHAPP_OPENAI_CHATGPT.py
+++ ENGINE/ALOHAPP_OPENAI_CHATGPT.py
@@ -1,22 +1,21 @@
 from openai import OpenAI
-
-
-
+from playsound import playsound
+import os
 from pathlib import Path
 p = Path.cwd()
 path_beginning = str(p.home())+'/PycharmProjects/OPENAI/'
 path = path_beginning+""
-
-import os
 cwd = os.getcwd()
+f = open(path+"account.txt", "r")
+key = f.read().strip()
+
 
 
 class ChatGPT():
     def __init__(self):
-        f = open(path+"account.txt", "r")
-        self.client = OpenAI(api_key=f.read())
+        self.client = OpenAI(api_key=key)
     def talk(self, text):
-        completion = self.client.chat.completions.create(model="gpt-3.5-turbo",
+        completion = self.client.chat.completions.create(model="gpt-3.5-turbo-0613",
         messages=[
             {"role": "user", "content": text}
         ])
