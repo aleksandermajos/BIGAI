fd94eb464a2cbf9c376af6714ff20a7a069559b0
aleksander majos <aleksander.majos@gmail.com>
2024-05-29 19:37:29 +0200

    WHISPERX_VAD TRY

--- ENGINE/PYAUDIO_DEVICES.py
+++ ENGINE/PYAUDIO_DEVICES.py
@@ -7,3 +7,4 @@ numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
