6236d69c6e6a092ee6d14d9a84fae5191d3eb7d2
aleksander majos <aleksander.majos@gmail.com>
2024-07-31 15:24:04 +0200

    BIGAI ALOHAPP SOURCE PICKLE

--- FRONT/ALOHAPP/SOURCE.py
+++ FRONT/ALOHAPP/SOURCE.py
@@ -2,6 +2,12 @@ from ENGINE.API_BIGAI_CLIENT import transcribe, tts_melo
 import pickle
 import spacy_stanza
 from pydub import AudioSegment
+from openai import OpenAI
+from playsound import playsound
+import os
+from pathlib import Path
+import pathlib
+
 
 
 
@@ -23,15 +29,12 @@ class SOURCE(object):
     pics = []
     videos = []
 
-    def populate_words_text(self):
-        pass
 
-    def __init__(self, name, path, source_type,user_type,part, language):
+    def __init__(self, name, path, source_type,user_type, language):
         self.name = name
         self.path = path
         self.source_type = source_type
         self.user_type = user_type
-        self.part = part
         self.language = language
         if self.source_type not in SOURCE.source_type:
             print(f'Source type {self.source_type} not supported')
@@ -39,8 +42,8 @@ class SOURCE(object):
 
 
     def populate_text(self):
-        lemma = spacy_stanza.load_pipeline("de")
-        text = transcribe(file_path=self.path, language='de')
+        lemma = spacy_stanza.load_pipeline(self.language)
+        text = transcribe(file_path=self.path, language=self.language)
         audio = AudioSegment.from_file(self.path)
         for segment in text['segments']:
             lem = lemma(segment['text'])
@@ -63,9 +66,22 @@ class SOURCE(object):
         return text
 
 
-    def populate_audio(self):
-        pass
 
+p = Path.cwd()
+path_data = str(p.home())+'/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/DE/LITTLE_PRINCE/'
+path_data = Path(path_data)
+files = [f for f in path_data.iterdir() if f.is_file()]
+
+for chapter_path in files:
+    file_name = chapter_path.name
+    path_str = str(chapter_path)
+    print(file_name)
+    current_chapter = SOURCE(name=file_name,path = path_str, source_type='AUDIO', user_type='BOOK', language='de')
+    result = current_chapter.populate_text()
+    with open(path_str+'.pkl', 'wb') as file:
+        pickle.dump(result, file)
+
+'''
 path =r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/DE/LITTLE_PRINCE/03 Der Kleine Prinz - Kapitel 1.mp3'
 LITTLEPRINCE_AUDIO_BOOK_PART1_DE = SOURCE(name='LITTLEPRINCE_AUDIO_BOOK_PART1_DE',path = path, source_type='AUDIO', user_type='BOOK', part=1, language='de')
 pick = LITTLEPRINCE_AUDIO_BOOK_PART1_DE.populate_text()
@@ -73,3 +89,4 @@ with open('my_pick.pkl', 'wb') as file:
     # Step 4: Use pickle.dump() to serialize and save the dictionary
     pickle.dump(pick, file)
 oko=5
+'''
