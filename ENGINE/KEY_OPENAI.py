455b98f5762923bbc949522d5d3a6b5f63f9991c
aleksander majos <aleksander.majos@gmail.com>
2024-09-27 21:40:42 +0200

    o1_preview

--- ENGINE/KEY_OPENAI.py
+++ ENGINE/KEY_OPENAI.py
@@ -23,3 +23,19 @@ def generate_and_play(text, voice, path=''):
     else:
         response.stream_to_file(path)
         playsound(path)
+
+def o1_preview():
+    response = client.chat.completions.create(
+        model="o1-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": "Write a bash script that takes a matrix represented as a string with format '[1,2],[3,4],[5,6]' and prints the transpose in the same format."
+            }
+        ]
+    )
+
+    print(response.choices[0].message.content)
+
+o1_preview()
+oko=5
