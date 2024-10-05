011541a236df2a8bba0fa5b2f4d32019cf51d9b9
aleksandermajos <aleksander.majos@gmail.com>
2024-10-05 23:04:01 +0200

    BIGAI FLUX

---
+++ ENGINE/GEN_PIC_FLUX.py
@@ -0,0 +1,15 @@
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+prompt = "A cat holding a sign that says hello world"
+image = pipe(
+    prompt,
+    guidance_scale=0.0,
+    num_inference_steps=4,
+    max_sequence_length=256,
+    generator=torch.Generator("mps:0").manual_seed(0)
+).images[0]
+image.save("flux-schnell.png")
