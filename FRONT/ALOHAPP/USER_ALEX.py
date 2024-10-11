93189f5989fc7d7df5cae99f03d0948d888a12e9
aleksander majos <aleksander.majos@gmail.com>
2024-10-11 23:05:59 +0200

    USER + INITIAL SOURCE HAS BEEN POPULATED.4000 + WORDS TOGETHER!!

--- FRONT/ALOHAPP/USER_ALEX.py
+++ FRONT/ALOHAPP/USER_ALEX.py
@@ -3,5 +3,21 @@ from SOURCE import SOURCE_CREATE, SOURCE
 
 Alex_user = USER(native='pl',langs=['fr'])
 Assimil_FR = SOURCE(source_type='AUDIO',user_type='BOOK',path=r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/FR/SELF_LEARNING/ASSIMIL')
-Alex_user.sources.append(Assimil_FR)
+LittlePrince_FR = SOURCE(source_type='AUDIO',user_type='BOOK',path=r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/FR/LITTLE_PRINCE')
+BlondynkaNew_FR = SOURCE(source_type='AUDIO',user_type='BOOK',path=r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/FR/SELF_LEARNING/BLONDYNA_NEW')
+BlondynkaOld_FR = SOURCE(source_type='AUDIO',user_type='BOOK',path=r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/FR/SELF_LEARNING/BLONDYNA_OLD')
+MowimyPoFrancusku_FR = SOURCE(source_type='AUDIO',user_type='BOOK',path=r'/home/bigai/PycharmProjects/BIGAI/DATA/ALOHAPP/AUDIO/BOOK/FR/SELF_LEARNING/MOWIMY_PO_FRANCUSKU')
+Alex_sources = [Assimil_FR,LittlePrince_FR,BlondynkaNew_FR,BlondynkaOld_FR,MowimyPoFrancusku_FR]
+
+all_words_in_sources = []
+for source in Alex_sources:
+    all_words_in_sources.append(source.all_words)
+all_words_in_sources = set().union(*all_words_in_sources)
+
+Alex_user.sources.extend(Alex_sources)
+
+
+
+
+
 oko = 4
