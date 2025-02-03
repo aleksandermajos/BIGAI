b9fccd67ab75a3ddaf965c77f3588c995d45723b
aleksandermajos <aleksander.majos@gmail.com>
2025-02-03 18:38:23 +0100

    ALOHAPP USER ZH

--- FRONT/ALOHAPP/ALOHAPP.py
+++ FRONT/ALOHAPP/ALOHAPP.py
@@ -9,18 +9,20 @@ from SOURCE import *
 def main(page: ft.Page):
     page.title = "ALOHAPP"
 
+    lang = 'zh'
+
     #page.user = USER(native='en',langs=['ja','zh'],langs_priority=['ja','zh'])
-    #with open("USER_ALEX_ASSIMIL_2_LESSONS_JA_ZH.pkl", "wb") as file:  # 'wb' means write in binary mode
+    #with open("USER_ALEX_ASSIMIL.pkl", "wb") as file:  # 'wb' means write in binary mode
         #pickle.dump(page.user, file)
 
     with open("USER_ALEX_ASSIMIL.pkl", 'rb') as file:  # 'rb' mode is for reading in binary
         page.user = pickle.load(file)
 
 
-    page.user.Update_Words_Present(source_name='ASSIMIL',source_lang='zh',start=0,end=1)
-    page.user.Create_Prompt_From_Words_Present(lang='zh')
+    page.user.Update_Words_Present(source_name='ASSIMIL',source_lang=lang,start=0,end=1)
+    page.user.Create_Prompt_From_Words_Present(lang=lang)
 
-    page.rows_full_words_button = generate_full_words_buttons_rows(user=page.user,lang='zh')
+    page.rows_full_words_button = generate_full_words_buttons_rows(user=page.user,lang=lang)
     page.words_column ,page.words_container = create_words_container(page.rows_full_words_button)
 
     page.conversation_column ,page.conversation_container = create_conversation_container()
