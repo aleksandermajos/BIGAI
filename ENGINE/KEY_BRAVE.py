3886ab726763d10c70173dfc392f8f7e0ae9e5c4
aleksandermajos <aleksander.majos@gmail.com>
2025-06-17 19:29:34 +0200

    LD DATA VIS

--- ENGINE/KEY_BRAVE.py
+++ ENGINE/KEY_BRAVE.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 p = Path.cwd()
-path_beginning = str(p.home())+'/PycharmProjects/BRAVE/'
+path_beginning = str(p.home())+'/PycharmProjects/BREAVE/'
 
 def provide_key():
     f = open(path_beginning+"account.txt", "r")
